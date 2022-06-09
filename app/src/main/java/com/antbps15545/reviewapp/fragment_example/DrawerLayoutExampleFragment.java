977fa0f4a58fd5f618d5d34b5dd0d65d11c30f5e
Betua3001 <93861161+Betua3001@users.noreply.github.com>
2022-06-09 17:54:28 +0700

    update

---
+++ app/src/main/java/com/antbps15545/reviewapp/fragment_example/DrawerLayoutExampleFragment.java
@@ -0,0 +1,74 @@
+package com.antbps15545.reviewapp.fragment_example;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+
+import com.antbps15545.reviewapp.R;
+import com.google.android.material.navigation.NavigationView;
+
+public class DrawerLayoutExampleFragment extends Fragment {
+
+    private DrawerLayout mDrawerLayout;
+    ActionBarDrawerToggle toggle;
+    Toolbar tool_bar;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_drawer_layout_example, container, false);
+
+        tool_bar = view.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar();
+
+        mDrawerLayout = view.findViewById(R.id.drawer_layout);
+        mDrawerLayout.openDrawer(GravityCompat.START);
+        toggle = new ActionBarDrawerToggle(getActivity(), mDrawerLayout, R.string.open_drawer, R.string.close_drawer);
+        mDrawerLayout.addDrawerListener(toggle);
+
+        NavigationView navigationView = view.findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.nav_camera:
+                        Toast.makeText(getContext(), "Import....", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_gallery:
+                        Toast.makeText(getContext(), "Gallery....", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_slideshow:
+                        Toast.makeText(getContext(), "Slideshow....", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.nav_manage:
+                        Toast.makeText(getContext(), "Manage....", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                item.setChecked(true);
+
+                mDrawerLayout.closeDrawers();
+
+                return true;
+            }
+        });
+
+        return view;
+
+    }
+}
