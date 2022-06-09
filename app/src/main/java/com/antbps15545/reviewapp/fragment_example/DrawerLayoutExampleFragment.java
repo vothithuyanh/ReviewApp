3fd0dc7a748b21745b1f966e33c53328c78a4397
baoan1501 <baoan3108nttv@gmail.com>
2022-06-09 18:19:13 +0700

    edit drawer menu example

--- app/src/main/java/com/antbps15545/reviewapp/fragment_example/DrawerLayoutExampleFragment.java
+++ app/src/main/java/com/antbps15545/reviewapp/fragment_example/DrawerLayoutExampleFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
@@ -14,7 +15,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
-import android.widget.Toolbar;
 
 
 import com.antbps15545.reviewapp.R;
@@ -31,15 +31,14 @@ public class DrawerLayoutExampleFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_drawer_layout_example, container, false);
-
         tool_bar = view.findViewById(R.id.toolbar);
-        ((AppCompatActivity)getActivity()).getSupportActionBar();
-
+        ((AppCompatActivity)getActivity()).setSupportActionBar(tool_bar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Drawer Example");
         mDrawerLayout = view.findViewById(R.id.drawer_layout);
         mDrawerLayout.openDrawer(GravityCompat.START);
-        toggle = new ActionBarDrawerToggle(getActivity(), mDrawerLayout, R.string.open_drawer, R.string.close_drawer);
+        toggle = new ActionBarDrawerToggle(getActivity(), mDrawerLayout, tool_bar, R.string.open_drawer, R.string.close_drawer);
         mDrawerLayout.addDrawerListener(toggle);
-
+        toggle.syncState();
         NavigationView navigationView = view.findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
