0c0c4086eaea3b308d3f5a5060d6d78ff059ff13
Betua3001 <93861161+Betua3001@users.noreply.github.com>
2022-06-14 08:54:13 +0700

    update
    Up phần alertdialog từ thiện

--- app/src/main/java/com/antbps15545/reviewapp/fragment_example/AlertDialogExampleFragment.java
+++ app/src/main/java/com/antbps15545/reviewapp/fragment_example/AlertDialogExampleFragment.java
@@ -1,50 +1,79 @@
 package com.antbps15545.reviewapp.fragment_example;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.Toast;
 
 
 import com.antbps15545.reviewapp.R;
 
 
-public class AlertDialogExampleFragment extends DialogFragment {
+public class AlertDialogExampleFragment extends Fragment {
 
-    @NonNull
+    Button btn;
+
+    @Nullable
     @Override
-    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        // Use the Builder class for convenient dialog construction
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage("Start game")
-                .setPositiveButton("Start", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // START THE GAME!
-                        Toast.makeText(getContext(), "Begin Game", Toast.LENGTH_LONG).show();
-                    }
-                })
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // User cancelled the dialog
-                        Toast.makeText(getContext(), "Cancel Game", Toast.LENGTH_LONG).show();
-                    }
-                });
-        // Create the AlertDialog object and return it
-        builder.create();
-        return builder.show();
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_alert_dialog_example, container, false);
+
+        btn = view.findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                exit(view);
+            }
+        });
+
+        return view;
+    }
+    public void exit(View view){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getContext());
+        // Setting Alert Dialog Title
+        alertDialogBuilder.setTitle("Xác nhận để thoát..!!!");
+        // Icon Of Alert Dialog
+        alertDialogBuilder.setIcon(R.drawable.question);
+        // Setting Alert Dialog Message
+        alertDialogBuilder.setMessage("Bạn có muốn thoát?");
+        alertDialogBuilder.setCancelable(false);
+
+        alertDialogBuilder.setPositiveButton("Đồng ý", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface arg0, int arg1) {
+                //Đóng Activity hiện tại
+                Toast.makeText(getContext(),"Bạn đã click vào nút đồng ý",Toast.LENGTH_SHORT).show();
+                getActivity().finish();
+            }
+        });
+
+        alertDialogBuilder.setNegativeButton("Không đồng ý", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getContext(),"Bạn đã click vào nút không đồng ý",Toast.LENGTH_SHORT).show();
+            }
+        });
+        alertDialogBuilder.setNeutralButton("Hủy", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getContext(),"Bạn đã click vào nút hủy",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
     }
 }
 
